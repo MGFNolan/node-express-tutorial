3cd85fc84aa3d135f4fd52bb070dca1e0b1bfd4f
Mattn <mattn@Esthers-Air.broadband>
2022-02-20 22:32:39 +0000

    add part 9 and 10

---
+++ 10-fs-sync.js
@@ -0,0 +1,14 @@
+//another way of accessing. Can still write const fs = require('fs')
+const {readFileSync, writeFileSync} =  require('fs');
+
+//remember that readFileSync is a function being called. It is accessed from fs above.
+const first = readFileSync('./content/first.txt', 'utf8');
+const second = readFileSync('./content/second.txt', 'utf8');
+
+
+//If the file is not there, writeFileSync will create it. If it DOES exist, it will OVERWRITE it
+//flag:'a' appends the file you are writing instead
+writeFileSync('./content/result-sync.txt', 
+`Here is the result: ${first}, ${second}`,
+{flag: 'a',}
+)
--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-const path = require('path');
+//another way of accessing. Can still write const fs = require('fs')
+const {readFile, writeFile} =  require('fs');
 
-console.log(path.sep);
+//We need to provide a callback. We will run that callback when we are done
+//An example of a callback function is like addEventListener. When we write
+//the function to tell it what to do, that's a callback function
 
-const filePath = path.join('/content', '/subfolder', 'test.txt');
-console.log(filePath);
-
-const base = path.basename(filePath);
-console.log(base);
-
-const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt')
-console.log(absolute)
+console.log('start')
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  if (err) {
+    console.log(err)
+    return
+  }
+  const first = result
+  readFile('./content/second.txt', 'utf8', (err, result) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    const second = result
+    writeFile(
+      './content/result-async.txt',
+      `Here is the result : ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err)
+          return
+        }
+        console.log('done with this task')
+      }
+    )
+  })
+})
+console.log('starting next task')
